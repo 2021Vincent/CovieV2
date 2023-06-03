836cad07e1373eb4ed11d399a4367c8e9f65b7b5
UG <ug00257793@gmail.com>
2023-06-03 23:39:12 +0800

    make new classify

--- src/lda_analyze.py
+++ src/lda_analyze.py
@@ -3,6 +3,7 @@ from gensim import corpora
 import json
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
+import numpy as np
 
 # import nltk
 # nltk.download('stopwords')
@@ -73,6 +74,7 @@ class lda_analyze:
     
     def combine_date_of_comment(self):
         N = 250
+        
         res = {}
         for i in range(1, N + 1):
             data = json.loads(open(f"data/Rank_{i}.json", "r").read())
@@ -100,23 +102,26 @@ class lda_analyze:
     
     def analyze_year_comment(self):
         years = [1990, 2000, 2010, 2020]
+        TOPIC_NUM = 50
         # years = [1990]
-        output_dict = {'1990': {'topic':[[] for i in range(100)]}, 
-                       '2000': {'topic':[[] for i in range(100)]}, 
-                       '2010': {'topic':[[] for i in range(100)]}, 
-                       '2020': {'topic':[[] for i in range(100)]}}
+        year_file_cnt = {1990: 2, 2000: 11, 2010: 13, 2020: 10}
+        # output_dict = {'1990': {'topic_num':TOPIC_NUM*year_file_cnt[1990], 'topic':[]}, 
+        #                '2000': {'topic_num':TOPIC_NUM*year_file_cnt[2000], 'topic':[]}, 
+        #                '2010': {'topic_num':TOPIC_NUM*year_file_cnt[2010], 'topic':[]}, 
+        #                '2020': {'topic_num':TOPIC_NUM*year_file_cnt[2020], 'topic':[]}}
         for year in years:
-            print(f"analyzing topic of year_{year}")
-            comments = json.loads(open(f"src/lda/lda_comment_{year}.json", "r").read())
+            # print(f"analyzing topic of year_{year}")
+            output_dict = {}
+            output_dict[str(year)] = {'topic_num':TOPIC_NUM*year_file_cnt[year], 'topic':[]}
             tokenizer = RegexpTokenizer(r"\w+")
             en_stop = stopwords.words("english")
-            doc = []
-            # for comment in comments:
-            #     doc.append(comment["comment"])
-            #     # print(comment["comment"])
-
-                
-            for comment in comments:
+            
+            for file_cnt in range(1, year_file_cnt[year] + 1):
+                print(f"analyzing topic of year_{year}_{file_cnt}")
+                comments = json.loads(open(f"src/lda/lda_comment_{year}_{file_cnt}.json", "r").read())
+            
+                doc = []
+                for comment in comments:
                     custom_stop_word = []
                     custom_stop_word.extend([i.lower() for i in comment["name"].split()])
                     custom_stop_word.extend([i.capitalize() for i in comment["name"].split()])
@@ -127,24 +132,30 @@ class lda_analyze:
                         ]
                     )
 
-            dic = corpora.Dictionary(doc)
-            corpus = [dic.doc2bow(doc) for doc in doc]
-            lda = gensim.models.LdaModel(
-                corpus=corpus,
-                id2word=dic,
-                num_topics=100,
-                random_state=100,
-                update_every=1,
-                passes=10,
-                alpha="auto",
-                per_word_topics=True,
-            )
+                dic = corpora.Dictionary(doc)
+                corpus = [dic.doc2bow(doc) for doc in doc]
+                lda = gensim.models.LdaModel(
+                    corpus=corpus,
+                    id2word=dic,
+                    num_topics=TOPIC_NUM*2,
+                    random_state=100,
+                    update_every=1,
+                    passes=10,
+                    per_word_topics=True
+                )
 
-            for topic in lda.show_topics(formatted=False):
-                for word, prob in topic[1]:
-                    output_dict[str(year)]['topic'][topic[0]].append(word)
+            # print(lda.show_topics(formatted=False))
+                for topic in lda.show_topics(num_topics = 50, formatted=False):
+                    output_dict[str(year)]['topic'].append([])
+                    for word, prob in topic[1]:
+                        # output_dict[str(year)]['topic'][topic[0]].append([word, float(prob)])
+                        output_dict[str(year)]['topic'][-1].append(word)
                 #     print(word, end=" ")
                 # print()
 
-        with open(f"src/lda/lda_comment_topic.json", "w") as outfile:
-            json.dump(output_dict, outfile)
+            with open(f"src/lda/lda_comment_topic_{year}.json", "w") as outfile:
+                json.dump(output_dict, outfile)
+
+
+if(__name__ == "__main__"):
+    lda_analyze().analyze_year_comment()
--- src/lda_classifiy.py
+++ src/lda_classifiy.py
@@ -25,8 +25,8 @@ class lda_classify:
             "dialog",
             "acting",
             "visual",
-            "count",
-            "per_topic",
+            "cat_total",
+            "topic_total",
         ]
 
     def sim(self, x, y):
@@ -49,76 +49,144 @@ class lda_classify:
         return 0
 
     def load_res(self):
-        self.res = json.loads(open("src/lda/lda_classify_result.json", "r").read())
+        self.res = json.loads(open("src/lda/lda_classify_comment_result.json", "r").read())
 
-    def run(self):
-        flair_embedding_forward = FlairEmbeddings("news-forward")
-        category_sentence = Sentence(self.category)
-        flair_embedding_forward.embed(category_sentence)
+    # def run(self):
+    #     flair_embedding_forward = FlairEmbeddings("news-forward")
+    #     category_sentence = Sentence(self.category)
+    #     flair_embedding_forward.embed(category_sentence)
 
-        res = {1970: {}, 1980: {}, 1990: {}, 2000: {}, 2010: {}, 2020: {}, 0: {}}
-        for cat in self.category:
-            for year in res:
-                res[year][cat] = 0
+    #     res = {1970: {}, 1980: {}, 1990: {}, 2000: {}, 2010: {}, 2020: {}, 0: {}}
+    #     for cat in self.category:
+    #         for year in res:
+    #             res[year][cat] = 0
 
-        N = 250
-        for i in range(1, N + 1):
-            print(f"classfy topics from rank_{i}")
-            data = json.loads(open(f"src/lda/Rank_{i}_lda.json", "r").read())
+    #     N = 250
+    #     for i in range(1, N + 1):
+    #         print(f"classfy topics from rank_{i}")
+    #         data = json.loads(open(f"src/lda/Rank_{i}_lda.json", "r").read())
 
-        for topic in data["topic"]:
-            sentence = Sentence(topic)
-            flair_embedding_forward.embed(sentence)
+    #     for topic in data["topic"]:
+    #         sentence = Sentence(topic)
+    #         flair_embedding_forward.embed(sentence)
 
-            topic_cat = []
+    #         topic_cat = []
 
-            for word in sentence:
-                word_emb = word.embedding
-                for cat in category_sentence:
-                    cat_emb = cat.embedding
-                    if (cat.text not in topic_cat) and (
-                        self.sim(word_emb, cat_emb) > 0.3
-                    ):
-                        topic_cat.append(cat.text)
-                for cat in topic_cat:
-                    res[self.get_years(data["year"])][cat] += 1
-                    res[self.get_years(data["year"])]["count"] += 1
-                if topic_cat:
-                    res[self.get_years(data["year"])]["per_topic"] += 1
+    #         for word in sentence:
+    #             word_emb = word.embedding
+    #             for cat in category_sentence:
+    #                 cat_emb = cat.embedding
+    #                 if (cat.text not in topic_cat) and (self.sim(word_emb, cat_emb) > 0.3):
+    #                     topic_cat.append(cat.text)
+                
+    #             for cat in topic_cat:
+    #                 res[self.get_years(data["year"])][cat] += 1
+    #                 res[self.get_years(data["year"])]["count"] += 1
+    #             if topic_cat:
+    #                 res[self.get_years(data["year"])]["per_topic"] += 1
 
-        # save the result
-        with open("src/lda/lda_classify_result.json", "w") as outfile:
-            json.dump(res, outfile)
-        self.res = res
+    #     # save the result
+    #     with open("src/lda/lda_classify_result.json", "w") as outfile:
+    #         json.dump(res, outfile)
+    #     self.res = res
 
     def show_res(self):
         df = pd.DataFrame(self.res)
-        df = df.drop('0', axis=1)
+        # df = df.drop('0', axis=1)
         print(df)
+
+
+        # bar chart for every year
+        the_category = ['genre', 'story', 'dialog', 'scene', 'character', 'actor', 'music']
+        year_color = {'1990': "#81e1b3", '2000': "#87c4f2", '2010': "#8189ff", '2020': "#b362f9"}
+        for year in df.head():
+            df[year][the_category].plot.bar(color = year_color[year])
+            plt.title(year)
+            plt.show()
+
 
         # make res into percentage
         for year in df.head():
             for cat in self.res[year]:
-                if cat == "count" or cat == "per_topic":
+                if cat == "topic_total" or cat == "cat_total":
                     continue
-                df.loc[cat, year] /= df.loc["per_topic", year]
+                df.loc[cat, year] /= df.loc["topic_total", year]
         print(df)
 
 
         # plot
-        for cat in self.category:
-            if cat == "count" or cat == "per_topic":
+        for cat in ['genre', 'story', 'dialog', 'scene', 'character', 'actor', 'music']:
+            if cat == "topic_total" or cat == "cat_total":
                 continue
             # limit the range of y axis
-            plt.ylim(0, 0.7)
-            df.loc[cat].plot(label=cat)
+            plt.ylim(0, 1)
+            df.loc[cat].plot(label=cat, marker = 'o')
             plt.title(cat)
             plt.show()
         # plt.legend()
         ["chararcter","genre","scene","actor","story","music","editing", "picture", "dialog", "acting"]
 
+    def run_by_year(self):
+        flair_embedding_forward = FlairEmbeddings("news-forward")
+        category_sentence = Sentence([i for i in self.category if i != "cat_total" and i != "topic_total"])
+        flair_embedding_forward.embed(category_sentence)
 
+        res = {1990: {}, 2000: {}, 2010: {}, 2020: {}}
+        for cat in self.category:
+            for year in res:
+                res[year][cat] = 0
+        
+        years = [1990, 2000, 2010, 2020]
+        # years = [1990]
+        TOPIC_NUM = 50
+        # year_file_cnt = {1990: 1, 2000: 11, 2010: 13, 2020: 10}
+
+
+
+        for year in years:
+            # for file_cnt in range(1, year_file_cnt[year]+1):
+            print(f"classfying topic of year_{year}")
+            data = json.loads(open(f"src/lda/lda_comment_topic_{year}.json", "r").read())
+
+            for topic in data[str(year)]['topic']:
+                sentence = Sentence(topic)
+                flair_embedding_forward.embed(sentence)
+
+                topic_cat = []
+                for word in sentence:
+                    word_emb = word.embedding
+                    for cat in category_sentence:
+                        cat_emb = cat.embedding
+                        if (cat.text not in topic_cat) and (self.sim(word_emb, cat_emb) > 0.45):
+                            topic_cat.append(cat.text)
+                        
+                for cat in topic_cat:
+                    res[year][cat] += 1
+                    res[year]["cat_total"] += 1
+                if topic_cat:
+                    res[year]["topic_total"] += 1
+
+        # save the result
+        with open("src/lda/lda_classify_comment_result.json", "w") as outfile:
+            json.dump(res, outfile)
+                            
 if __name__ == "__main__":
     lda_classify = lda_classify()
+    # lda_classify.run_by_year()
     lda_classify.load_res()
     lda_classify.show_res()
+
+'''
+['genre', 'story', 'dialog', 'scene', 'character', 'actor', 'music']
+
+
+[ girl,      possessed, Vincent,   knight, God, 
+  encourage, Malcom,    possesses, kills,  Cole ]
+
+[ Mr,     super,  Incredible, family, hero, 
+  powers, heroes, Fantastic,  life,   Four ]
+
+
+[ movie, This, film, It, I, one, Death, A, best, see ]
+[ I, movie, see, time, one, It, seen, like, movies, This ]
+'''
