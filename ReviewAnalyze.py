0d5632e9eba1c77c3f5f8cb173cefe585c447a30
catJam <loginerrore04@gmail.com>
2023-06-14 11:35:28 +0800

    update

--- ReviewAnalyze.py
+++
@@ -1,7 +0,0 @@
-from flair.nn import Classifier
-from flair.data import Sentence
-
-classifier = Classifier.load('sentiment')
-sentence = Sentence("I had heard that Holy Grail was a hilarious flick, so I was pretty excited as the opening credits rolled by. It was supposedly \"the epitome of British comedy\". If this is the best that British comedy has to offer, I think I'll stay American, thank you. The movie was absolutely stupid. Now, I like certain types of stupid comedy, but this movie just didn't cut it. It was a waste of my time. If you want to see real British comedy, check out the old game show \"Whose Line is it Anyway?\" If you want to see garbage put on film, Holy Grail is the movie for you.")
-classifier.predict(sentence)
-print(sentence)
